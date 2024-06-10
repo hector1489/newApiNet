77ce3d913815557139dc78fe81bdfd53e0c74f91
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-10 11:06:24 -0400

    mapGroup

--- Program.cs
+++ Program.cs
@@ -1,59 +1,75 @@
-using Microsoft.EntityFrameworkCore;
 using NSwag.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
 {
-    config.DocumentName = "TodoAPI";
-    config.Title = "TodoAPI v1";
-    config.Version = "v1";
+  config.DocumentName = "TodoAPI";
+  config.Title = "TodoAPI v1";
+  config.Version = "v1";
 });
+
 var app = builder.Build();
 
-app.MapGet("/todoitems", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
-
-app.MapGet("/todoitems/complete", async (TodoDb db) =>
-    await db.Todos.Where(t => t.IsComplete).ToListAsync());
-
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
-    await db.Todos.FindAsync(id)
-        is Todo todo
-            ? Results.Ok(todo)
-            : Results.NotFound());
-
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+if (app.Environment.IsDevelopment())
 {
-    db.Todos.Add(todo);
-    await db.SaveChangesAsync();
+  app.UseOpenApi();
+  app.UseSwaggerUi(config =>
+  {
+    config.DocumentTitle = "TodoAPI";
+    config.Path = "/swagger";
+    config.DocumentPath = "/swagger/{documentName}/swagger.json";
+    config.DocExpansion = "list";
+  });
+}
 
-    return Results.Created($"/todoitems/{todo.Id}", todo);
+var todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", async (TodoDb db) =>
+  await db.Todos.ToListAsync());
+
+todoItems.MapGet("/complete", async (TodoDb db) =>
+  await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+todoItems.MapGet("/{id}", async (int id, TodoDb db) =>
+  await db.Todos.FindAsync(id)
+    is Todo todo
+      ? Results.Ok(todo)
+      : Results.NotFound());
+
+todoItems.MapPost("/", async (Todo todo, TodoDb db) =>
+{
+  db.Todos.Add(todo);
+  await db.SaveChangesAsync();
+
+  return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+todoItems.MapPut("/{id}", async (int id, Todo inputTodo, TodoDb db) =>
 {
-    var todo = await db.Todos.FindAsync(id);
+  var todo = await db.Todos.FindAsync(id);
 
-    if (todo is null) return Results.NotFound();
+  if (todo is null) return Results.NotFound();
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+  todo.Name = inputTodo.Name;
+  todo.IsComplete = inputTodo.IsComplete;
 
-    await db.SaveChangesAsync();
+  await db.SaveChangesAsync();
 
-    return Results.NoContent();
+  return Results.NoContent();
 });
 
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+todoItems.MapDelete("/{id}", async (int id, TodoDb db) =>
 {
     if (await db.Todos.FindAsync(id) is Todo todo)
     {
-        db.Todos.Remove(todo);
-        await db.SaveChangesAsync();
-        return Results.NoContent();
+      db.Todos.Remove(todo);
+      await db.SaveChangesAsync();
+      return Results.NoContent();
     }
 
     return Results.NotFound();
