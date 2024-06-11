33376eeb440c654ac85c73d9c01c770f38b7e10b
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-11 18:05:09 -0400

    item dto commit

--- TodoItemDTO.cs
+++ TodoItemDTO.cs
@@ -1,11 +1,14 @@
 
+// Definición de la clase TodoItemDTO
+
 public class TodoItemDTO
 {
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
+  public int Id { get; set; }
+  public string? Name { get; set; }
+  public bool IsComplete { get; set; }
 
-    public TodoItemDTO() { }
-    public TodoItemDTO(Todo todoItem) =>
-    (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
+  public TodoItemDTO() { }
+  public TodoItemDTO(Todo todoItem) =>
+  (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
 }
+
